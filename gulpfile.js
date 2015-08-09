f93be782b4d2b4b5aa1cac504e2ae4fba800202d
John Morton <john@johnfmorton.com>
2015-08-09 17:04:31 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// dependencies
+
+var gulp = require('gulp');
+var processhtml = require('gulp-processhtml');
+var concat = require('gulp-concat');
+
+/* options for processhtml */
+var processhtmloptions = {
+    list: "replacement.list"
+};
+
